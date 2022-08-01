3dd563162f5cafba7997815ca54754d6b93ce8ea
Edita2 <edita.bark@icloud.com>
2022-08-01 22:22:19 +0300

    modal via portal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div>
+    <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
       </main>
-    </div>
+    </Fragment>
   );
 }
 
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -9,19 +10,19 @@ const Cart = (props) => {
     </ul>
   );
 
-  return;
-  <div>
-    {cartItems}
-
-    <div className={classes.total}>
-      <span>Total Amount</span>
-      <span>35.62</span>
-    </div>
-    <div className={classes.actions}>
-      <button className={classes["button--alt"]}>Close</button>
-      <button className={classes.button}>Order</button>
-    </div>
-  </div>;
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
 };
 
 export default Cart;
